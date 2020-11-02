74ad9eb645bb0d3daecc26bdd2a67665eb6c5c80
angel <angel@192.168.1.7>
2020-11-02 13:21:04 -0800

    second commit

--- src/TestClass1.java
+++ src/TestClass1.java
@@ -4,7 +4,11 @@ public class TestClass1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is first git project");
-
+		System.out.println("new code added");
+		int a = 10;
+		if(a<20) {
+			System.out.println("a<20");
+		}
 	}
 
 }
